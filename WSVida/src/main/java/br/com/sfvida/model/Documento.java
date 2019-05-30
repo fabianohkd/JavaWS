6aeffb221724769107c30c418c96e158eb09510d
fabianohkd <fabianoconrado@gmail.com>
2019-05-29 23:24:43 -0300

    Alterando o modelo para teste

--- WSVida/src/main/java/br/com/sfvida/model/Documento.java
+++ WSVida/src/main/java/br/com/sfvida/model/Documento.java
@@ -2,4 +2,14 @@ package br.com.sfvida.model;
 
 public class Documento {
 
+	private String numero;
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
 }
